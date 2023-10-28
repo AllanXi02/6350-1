76098567e559ef7eea68c9d2f38f1576feeee7e0
AllanXi02 <113737693+AllanXi02@users.noreply.github.com>
2023-10-28 01:16:20 -0700

    Add files via upload

---
+++ StockApp/Ultils/Constraints.swift
@@ -0,0 +1,12 @@
+//
+//  Constraints.swift
+//  StockApp
+//
+//  Created by yue xi on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "74b211a19381a4d74be23215bdc8cae8"
+
+let baseURL  = "https://financialmodelingprep.com/api/v3/"
